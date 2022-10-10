88e2e4798d25b6f1fc8b97d98444f2c5f43324f4
tamarajokic6 <tamarajokic6gmail.com>
2022-10-11 00:01:19 +0200

    Finished test

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,105 +1,88 @@
 package tests;
 
-import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pages.LoginPage;
 
 public class LoginTest extends BaseTest {
 
 
     @Test
-    public void test1() {
+
+    public void visitTheLoginPage() {
         homePage.goToLoginPage();
         String expectedResult = "/login";
-        String actualResult = loginPage.getDriver().getCurrentUrl().toString();
+        String actualResult = loginPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult.contains(expectedResult));
     }
 
     @Test
-    public void test2() {
+    public void checkInputTypes() {
         homePage.goToLoginPage();
         String expectedResult = "email";
-        String actualResult = loginPage.getDriver().findElement(By.id("email")).getAttribute("type").toString();
+        String actualResult = loginPage.getDriver().findElement(By.id("email")).getAttribute("type");
         Assert.assertEquals(expectedResult, actualResult);
         String expectedResult1 = "password";
-        String actualResult1 = loginPage.getDriver().findElement(By.id("password")).getAttribute("type").toString();
+        String actualResult1 = loginPage.getDriver().findElement(By.id("password")).getAttribute("type");
         Assert.assertEquals(expectedResult1, actualResult1);
     }
 
     @Test
-    public void test3() {
+    public void displayErrorsWhenUserDoesNotExist() {
         homePage.goToLoginPage();
         String expectedResult = "User does not exists";
-        faker = new Faker();
-        String emailFaker = faker.address().toString() + "@gmail.com";
-        String passwordFaker = faker.number().toString();
+        String emailFaker = faker.internet().emailAddress();
+        String passwordFaker = faker.internet().password();
         loginPage.login(emailFaker, passwordFaker);
-        String actualResult = loginPage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div")).getText();
+        String actualResult = loginPage.getMessage().getText();
+        Assert.assertTrue(actualResult.contains(expectedResult));
         String expectedResult1 = "/login";
-        String actualResult1 = loginPage.getDriver().getCurrentUrl().toString();
+        String actualResult1 = loginPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult1.contains(expectedResult1));
 
     }
 
     @Test
-    public void test4() {
+    public void displayErrorsWhenPasswordIsWrong() {
         homePage.goToLoginPage();
         String expectedResult = "Wrong password";
-        faker = new Faker();
-        String passwordFaker = faker.number().toString();
+        String passwordFaker = faker.internet().password();
         loginPage.login("admin@admin.com", passwordFaker);
-        String actualResult = loginPage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div")).getText();
+        String actualResult = loginPage.getMessage().getText();
+        Assert.assertTrue(actualResult.contains(expectedResult));
         String expectedResult1 = "/login";
-        String actualResult1 = loginPage.getDriver().getCurrentUrl().toString();
+        String actualResult1 = loginPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult1.contains(expectedResult1));
     }
 
     @Test
-    public void test5() {
+    public void login() {
         homePage.goToLoginPage();
         String expectedResult = "/home";
-        loginPage.login("admin@admin.com", "12345");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        String actualResult = loginPage.getDriver().getCurrentUrl().toString();
+        loginPage.validLogin();
+        wait.until(ExpectedConditions.urlContains("https://vue-demo.daniel-avellaneda.com/home"));
+        String actualResult = loginPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult.contains(expectedResult));
     }
 
     @Test
-    public void test6() {
-
+    public void logout() {
+        loginPage.loginToLogout();
         homePage.goToLoginPage();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        loginPage.login("admin@admin.com", "12345");
+        wait.until(ExpectedConditions.urlContains("https://vue-demo.daniel-avellaneda.com/login"));
+        loginPage.validLogin();
         String expectedResult = "LOGOUT";
-
-        String actualResult = loginPage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]")).getText();
+        String actualResult = loginPage.getLogoutButton().getText();
         Assert.assertTrue(actualResult.contains(expectedResult));
-        loginPage.logout();
+        loginPage.loginToLogout();
         String expectedResult1 = "/login";
-        String actualResult1 = loginPage.getDriver().getCurrentUrl().toString();
+        String actualResult1 = loginPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult1.contains(expectedResult1));
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+
         driver.get("https://vue-demo.daniel-avellaneda.com/home");
         String expectedResult2 = "/login";
-        String actualResult2 = loginPage.getDriver().getCurrentUrl().toString();
+        String actualResult2 = loginPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult2.contains(expectedResult2));
     }
 
