fe4fc0573fe9cb6c22397df88315ce99806d80c5
tamarajokic6 <tamarajokic6gmail.com>
2022-10-11 00:24:21 +0200

    Finished test

--- src/test/java/tests/SignUpTest.java
+++ src/test/java/tests/SignUpTest.java
@@ -1,52 +1,55 @@
 package tests;
 
-import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignUpTest extends BaseTest {
     @Test
-    public void test1() {
+    public void goToSignupPage() {
         homePage.goToSignUpPage();
         String expectedResult = "/signup";
-        String actualResult = signUpPage.getDriver().getCurrentUrl().toString();
+        String actualResult = signUpPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult.contains(expectedResult));
     }
 
     @Test
-    public void test2() {
+    public void checkInputTypes() {
         homePage.goToSignUpPage();
         String expectedResult = "email";
-        String actualResult = signUpPage.getDriver().findElement(By.id("email")).getAttribute("type").toString();
+        String actualResult = signUpPage.getDriver().findElement(By.id("email")).getAttribute("type");
         Assert.assertEquals(expectedResult, actualResult);
         String expectedResult1 = "password";
-        String actualResult1 = signUpPage.getDriver().findElement(By.id("password")).getAttribute("type").toString();
+        String actualResult1 = signUpPage.getDriver().findElement(By.id("password")).getAttribute("type");
         Assert.assertEquals(expectedResult1, actualResult1);
         String expectedResult2 = "password";
-        String actualResult2 = signUpPage.getDriver().findElement(By.id("confirmPassword")).getAttribute("type").toString();
+        String actualResult2 = signUpPage.getDriver().findElement(By.id("confirmPassword")).getAttribute("type");
         Assert.assertEquals(expectedResult2, actualResult2);
     }
 
     @Test
-    public void test3() {
+    public void displayErrorsWhenUserAlreadyExists() {
         homePage.goToSignUpPage();
         String expectedResult = "E-mail already exists";
         signUpPage.signUp("Test Test", "admin@admin.com", "123654", "123654");
-        String actualResult = signUpPage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[3]/div/div/div/div")).getText();
+        String actualResult = signUpPage.getMessage().getText();
+        Assert.assertTrue(actualResult.contains(expectedResult));
         String expectedResult1 = "/signup";
-        String actualResult1 = signUpPage.getDriver().getCurrentUrl().toString();
+        String actualResult1 = signUpPage.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult1.contains(expectedResult1));
     }
 
     @Test
-    public void test4() {
+    public void signup() {
 
         homePage.goToSignUpPage();
-        faker = new Faker();
         String emailFaker = faker.internet().emailAddress();
         String expectedResult = "IMPORTANT: Verify your account";
         signUpPage.signUp("Tamara Jokic", emailFaker, "123654", "123654");
-        String actualResult = signUpPage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[4]/div/div/div[1]")).getText();
+        wait.until(ExpectedConditions.textToBe(By.xpath("//*[@id=\"app\"]/div[4]/div/div/div[1]"), "IMPORTANT: Verify your account"));
+        String actualResult = signUpPage.getImportantMessage().getText();
+        Assert.assertTrue(actualResult.contains(expectedResult));
+
     }
 }
