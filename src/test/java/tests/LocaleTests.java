669237fcb97ff9e86d093784a3a2016d0f2b90c2
tamarajokic6 <tamarajokic6gmail.com>
2022-10-11 01:07:31 +0200

    Finished test

---
+++ src/test/java/tests/LocaleTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LocaleTests extends BaseTest {
+    @Test
+    public void verifyThatLocaleIsEs() {
+        localePage.esLanguage();
+        String expectedResult = "Página de aterrizaje";
+        String actualResult = homePage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")).getText();
+        Assert.assertTrue(actualResult.contains(expectedResult));
+    }
+
+    @Test
+    public void verifyThatLocaleIsEn() {
+        localePage.enLanguage();
+        String expectedResult = "Landing";
+        String actualResult = homePage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")).getText();
+        Assert.assertTrue(actualResult.contains(expectedResult));
+    }
+
+    @Test
+    public void verifyThatLocaleIsFr() {
+        localePage.frLanguage();
+        String expectedResult = "Page d'atterrissage";
+        String actualResult = homePage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")).getText();
+        Assert.assertTrue(actualResult.contains(expectedResult));
+    }
+}
