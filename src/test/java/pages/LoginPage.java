a3febd82f134f9bb6866c316c9de3becd35df625
tamarajokic6 <tamarajokic6gmail.com>
2022-10-11 14:54:09 +0200

    Finished test

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class LoginPage extends BasePage {
     private By emailField = By.id("email");
     private By passwordField = By.id("password");
     private By loginButton = By.xpath("//*[@id=\"app\"]/div/main/div/div[2]/div/div/div[3]/span/form/div/div[3]/button");
-private By logoutButton = By.xpath("//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]");
+    private By logoutButton = By.xpath("//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]");
+    private By message = By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div");
+
     public LoginPage(WebDriver driver) {
         super(driver);
     }
@@ -29,13 +33,28 @@ private By logoutButton = By.xpath("//*[@id=\"app\"]/div[1]/div/header/div/div[3
         return getDriver().findElement(logoutButton);
     }
 
+    public WebElement getMessage() {
+        return getDriver().findElement(message);
+    }
+
     public void login(String email, String password) {
         getEmailField().sendKeys(email);
         getPasswordField().sendKeys(password);
         getLoginButton().click();
-
-    } public void logout(){
-        getLogoutButton().click();
     }
 
+    public void validLogin() {
+        getEmailField().sendKeys("admin@admin.com");
+        getPasswordField().sendKeys("12345");
+        getLoginButton().click();
+    }
+
+    public void loginToLogout() {
+        List<WebElement> logout = getDriver().findElements(logoutButton);
+        if (logout.size() == 1) {
+            logout.get(0).click();
+        }
+    }
 }
+
+
