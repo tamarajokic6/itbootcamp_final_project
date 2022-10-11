565c980653e164c80f2b85da5e4c8fcb80cf8cc2
tamarajokic6 <tamarajokic6gmail.com>
2022-10-11 14:54:45 +0200

    Finished test

--- src/test/java/pages/SignUpPage.java
+++ src/test/java/pages/SignUpPage.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class SignUpPage extends BasePage{
+public class SignUpPage extends BasePage {
     private By name = By.id("name");
     private By email = By.id("email");
     private By password = By.id("password");
     private By confirmPassword = By.id("confirmPassword");
     private By signUpButton = By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[2]/span/form/div/div[5]/button");
+    private By message = By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[3]/div/div/div/div");
+    private By importantMessage = By.xpath("//*[@id=\"app\"]/div[4]/div/div/div[1]");
 
     public SignUpPage(WebDriver driver) {
         super(driver);
@@ -33,7 +35,18 @@ public class SignUpPage extends BasePage{
 
     public WebElement getSignUpButton() {
         return getDriver().findElement(signUpButton);
-    } public void signUp(String name,String email, String password, String confirm ){
+    }
+
+    public WebElement getMessage() {
+        return getDriver().findElement(message);
+    }
+
+    public WebElement getImportantMessage() {
+        return getDriver().findElement(importantMessage);
+    }
+
+    public void signUp(String name, String email, String password, String confirm) {
+
         getName().sendKeys(name);
         getEmail().sendKeys(email);
         getPassword().sendKeys(password);
