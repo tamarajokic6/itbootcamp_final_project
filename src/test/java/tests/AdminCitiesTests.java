341658fae0ddba5fc9d4fa33c0531023c87a1d23
tamarajokic6 <tamarajokic6gmail.com>
2022-10-12 17:53:52 +0200

    Finished test

--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -8,12 +8,14 @@ import org.testng.annotations.Test;
 public class AdminCitiesTests extends BaseTest {
     @Test
     public void adminCitiesPage() {
+
         homePage.goToLoginPage();
         loginPage.validLogin();
         adminCities.goToCities();
         String expectedResult = "/admin/cities";
         String actualResult = adminCities.getDriver().getCurrentUrl();
         Assert.assertTrue(actualResult.contains(expectedResult));
+
         String expectedResult1 = "LOGOUT";
         String actualResult1 = loginPage.getLogoutButton().getText();
         Assert.assertTrue(actualResult1.contains(expectedResult1));
@@ -21,11 +23,13 @@ public class AdminCitiesTests extends BaseTest {
 
     @Test
     public void createNewCity() {
+
         loginPage.loginToLogout();
         homePage.goToLoginPage();
         loginPage.validLogin();
         adminCities.goToCities();
         adminCities.addCity();
+
         String expectedResult = "Saved successfully";
         String actualResult = adminCities.getSavedSuccessfully().getText();
         Assert.assertTrue(actualResult.contains(expectedResult));
@@ -33,11 +37,13 @@ public class AdminCitiesTests extends BaseTest {
 
     @Test
     public void editCity() {
+
         loginPage.loginToLogout();
         homePage.goToLoginPage();
         loginPage.validLogin();
         adminCities.goToCities();
         adminCities.editCity();
+
         String expectedResult = "Saved successfully";
         String actualResult = adminCities.getSavedSuccessfully().getText();
         Assert.assertTrue(actualResult.contains(expectedResult));
@@ -56,6 +62,7 @@ public class AdminCitiesTests extends BaseTest {
         adminCities.editCity();
         wait.until(ExpectedConditions.textToBe(By.xpath("//*[@id=\"app\"]/div/main/div/div[2]/div/div[1]/div[2]/table/tbody/tr[1]/td[2]"), "Bajina Basta - edited"));
         adminCities.searchCity("Bajina Basta - edited");
+
         String expectedResult = "Bajina Basta - edited";
         String actualResult = adminCities.getFirstRow().getText();
         Assert.assertEquals(actualResult, expectedResult);
@@ -63,6 +70,7 @@ public class AdminCitiesTests extends BaseTest {
 
     @Test
     public void deleteCity() {
+
         loginPage.loginToLogout();
         homePage.goToLoginPage();
         loginPage.validLogin();
@@ -70,10 +78,11 @@ public class AdminCitiesTests extends BaseTest {
         adminCities.editCity();
         wait.until(ExpectedConditions.textToBe(By.xpath("//*[@id='app']/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]"), "Saved successfully\nCLOSE"));
         adminCities.searchCity("Bajina Basta - edited");
-        String expectedResult = "Deleted successfully";
         adminCities.getDeleteButton().click();
         adminCities.getDefinetelyDeleteButton().click();
         wait.until(ExpectedConditions.textToBe(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]"), "Deleted successfully\nCLOSE"));
+
+        String expectedResult = "Deleted successfully";
         String actualResult = adminCities.getSaveMessage().getText();
         Assert.assertTrue(actualResult.contains(expectedResult));
     }
