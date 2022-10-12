093d491d3ab49ea3e6059254142248d442d4d988
tamarajokic6 <tamarajokic6gmail.com>
2022-10-12 18:11:52 +0200

    Finished test

--- src/test/java/tests/AuthRoutesTest.java
+++ src/test/java/tests/AuthRoutesTest.java
@@ -3,7 +3,7 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class AuthRoutesTests extends BaseTest {
+public class AuthRoutesTest extends BaseTest {
     @Test
     public void forbidsVisitsToHomeUrlIfNotAuthenticated() {
 
