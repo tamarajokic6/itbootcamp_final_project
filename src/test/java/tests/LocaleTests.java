9d8082ee89ddcf137d310c2367065ceb998a5feb
tamarajokic6 <tamarajokic6gmail.com>
2022-10-12 18:03:49 +0200

    Finished test

--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -7,7 +7,9 @@ import org.testng.annotations.Test;
 public class LocaleTests extends BaseTest {
     @Test
     public void localeToEs() {
+
         localePage.esLanguage();
+
         String expectedResult = "Página de aterrizaje";
         String actualResult = homePage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")).getText();
         Assert.assertTrue(actualResult.contains(expectedResult));
@@ -15,7 +17,9 @@ public class LocaleTests extends BaseTest {
 
     @Test
     public void localeToEn() {
+
         localePage.enLanguage();
+
         String expectedResult = "Landing";
         String actualResult = homePage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")).getText();
         Assert.assertTrue(actualResult.contains(expectedResult));
@@ -23,7 +27,9 @@ public class LocaleTests extends BaseTest {
 
     @Test
     public void localeToFr() {
+
         localePage.frLanguage();
+
         String expectedResult = "Page d'atterrissage";
         String actualResult = homePage.getDriver().findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")).getText();
         Assert.assertTrue(actualResult.contains(expectedResult));
